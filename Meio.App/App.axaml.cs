63deb3d9706666baa01f0ea6f519a9d6ac783015
Kevan <selk11@proton.me>
2025-12-16 08:29:40 +0100

    Revert "✨ Application UI"

--- Meio.App/App.axaml.cs
+++ Meio.App/App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Meio.app.Views;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using PrettyLogging.Console;
--- Meio.App/Controls/HomeBar.axaml.cs
+++
@@ -1,7 +0,0 @@
-using Avalonia.Controls.Primitives;
-
-namespace Meio.app.Controls;
-
-public class HomeBar : TemplatedControl
-{
-}
